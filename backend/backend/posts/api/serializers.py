b0896407dffd7424371a4a0c37d65bf043b6ca82
ricardocanedo <ricardocanedo@live.com>
2025-04-06 15:04:48 -0300

    fix serializers

--- backend/backend/posts/api/serializers.py
+++ backend/backend/posts/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from ..models import Post
 
-class PostSerializer():
+class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
-        fields = ('__all__')
+        fields = ('id', 'title', 'content')
--- backend/backend/posts/models.py
+++ backend/backend/posts/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    # author = models.CharField(max_length=100)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Post: {self.title} by {self.author}"
+        return f"Post: {self.title}"
