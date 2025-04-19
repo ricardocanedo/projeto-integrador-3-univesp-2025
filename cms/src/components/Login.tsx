bc5efe83fbeefb5147094574d2dace23cf08f9ec
ricardocanedo <ricardocanedo@live.com>
2025-04-19 16:57:11 -0300

    add bootstrap

--- cms/src/components/Login.tsx
+++ cms/src/components/Login.tsx
@@ -22,28 +22,44 @@ const Login = () => {
 
     return (
         <div className="login-container">
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Username: </label>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
+            <h2 className='m-4'>Login</h2>
+            <form onSubmit={handleSubmit} className='form'>
+                <div className='form-group row mt-4'>
+                    <label className='col-sm-4 col-form-label'>Username: </label>
+                    <div className="col-sm-8 my-2">
+                        <input
+                            type="text"
+                            className='form-control'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label>Password: </label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+                <div className='form-group row'>
+                    <label className='col-sm-4 col-form-label'>Password: </label>
+                    <div className="col-sm-8 my-2">
+                        <input
+                            type="password"
+                            className="form-control"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
                 </div>
-                {error && <p style={{ color: 'red' }}>{error}</p>}
-                <button type="submit">Login</button>
+                <div className="m-2">
+                    {error && <p style={{ color: 'red' }}>{error}</p>}
+                </div>
+                <div className='form-group row my-4'>
+                    <div className="col-sm-12">
+                        <button type="submit"
+                            className='btn btn-warning w-100 mx-auto'>
+                            Login
+                        </button>
+                    </div>
+                </div>
             </form>
         </div>
+
     );
 };
 
