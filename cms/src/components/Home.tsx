4b52bf731e5e0f0840c525e0f158a246c0a6217d
ricardocanedo <ricardocanedo@live.com>
2025-04-19 16:58:32 -0300

    bem-vindo

--- cms/src/components/Home.tsx
+++ cms/src/components/Home.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="home-container">
-            <h1>Bem vindo</h1>
-            <p>Gerencie o conteúdo do seu sistema de forma eficiente.</p>
+            <h1>Bem-vindo</h1>
+            <p className='m-4'>Gerencie o conteúdo do seu sistema de forma eficiente.</p>
         </div>
     );
 };
