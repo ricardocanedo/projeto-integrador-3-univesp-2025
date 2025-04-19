e4e35ce1609979f97b789b8579ad5d7d04fbbc58
ricardocanedo <ricardocanedo@live.com>
2025-04-19 16:35:39 -0300

    bibliotecas, Home e Login

--- cms/src/App.tsx
+++ cms/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState, useEffect } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
 
 import './App.css'
 
 function App() {
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    async function fetchData() {
-      console.log(import.meta.env.VITE_API_URL)
-      try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}posts`)
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const result = await response.json();
-        console.log(result);
-        setData(result);
-
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
-
-    fetchData();
-  }, [])
-
-  return (
-    <>
-      Hello Vite + React!
-    </>
-  )
-}
+    return (
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              {/* ...other routes... */}
+          </Routes>
+      </Router>
+  );
+};
 
 export default App
---
+++ cms/src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h1>Bem vindo</h1>
+            <p>Gerencie o conteúdo do seu sistema de forma eficiente.</p>
+        </div>
+    );
+};
+
+export default Home;
