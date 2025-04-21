35648dd322c1461daa65a52dbd74edd63574ee2c
ricardocanedo <ricardocanedo@live.com>
2025-04-21 11:10:49 -0300

    create ProtectedRoute and add components

--- cms/src/App.tsx
+++ cms/src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
+import ProtectedPage from './components/ProtectedPage';
+import Dashboard from './components/Dashboard';
+import Posts from './components/Posts';
+import ProtectedRoute from './components/ProtectedRoute';
 
 import './App.css'
-import ProtectedPage from './components/ProtectedPage';
 
 function App() {
     return (
@@ -12,7 +15,30 @@ function App() {
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/protected" element={<ProtectedPage />} />
+              <Route
+                  path="/protected"
+                  element={
+                      <ProtectedRoute>
+                          <ProtectedPage />
+                      </ProtectedRoute>
+                  }
+              />
+              <Route
+                  path="/dashboard"
+                  element={
+                      <ProtectedRoute>
+                          <Dashboard />
+                      </ProtectedRoute>
+                  }
+              />
+              <Route
+                  path="/posts"
+                  element={
+                      <ProtectedRoute>
+                          <Posts />
+                      </ProtectedRoute>
+                  }
+              />
           </Routes>
       </Router>
   );
