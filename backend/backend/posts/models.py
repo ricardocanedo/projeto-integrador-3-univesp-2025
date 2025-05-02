41d86df6dbecccf6953549ecfb4aaac4351a39ee
ricardocanedo <ricardocanedo@live.com>
2025-05-02 10:13:43 -0300

    blog details dinamic

--- backend/backend/posts/models.py
+++ backend/backend/posts/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
     content = models.TextField()
     author = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Post: {self.title} - by {self.author}"
--- backend/backend/website/views.py
+++ backend/backend/website/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from posts.models import Post
 
 def home(request):
     return render(request, 'website/home.html')
@@ -7,10 +8,12 @@ def area_de_atuacao(request):
     return render(request, 'website/area_de_atuacao.html')
 
 def blog_list(request):
-    return render(request, 'website/blog_list.html')
+    posts = Post.objects.all()
+    return render(request, 'website/blog_list.html', {'posts': posts})
 
 def blog_detail(request, slug):
-    return render(request, 'website/blog_detail.html', {'slug': slug})
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'website/blog_detail.html', {'post': post})
 
 def politica_de_privacidade(request):
     return render(request, 'website/politica_de_privacidade.html')
