856ce1dda196bc0e38fe6ec375a0b3c6eedfc420
ricardocanedo <ricardocanedo@live.com>
2025-05-02 14:12:49 -0300

    post authenticated

--- backend/backend/posts/api/viewrs.py
+++ backend/backend/posts/api/viewrs.py
@@ -1,7 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from ..models import Post
 from .serializers import PostSerializer
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [AllowAny()]
+        return [IsAuthenticated()]
