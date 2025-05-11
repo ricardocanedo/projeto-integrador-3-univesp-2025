0a5d35f7fc4d36d8b1e24d0bae853b3e7ff5199d
ricardocanedo <ricardocanedo@live.com>
2025-05-11 19:48:47 -0300

    update links

--- backend/backend/website/urls.py
+++ backend/backend/website/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home', views.home, name='home'),
     path('area-de-atuacao/', views.area_de_atuacao, name='area_de_atuacao'),
     path('blog/', views.blog_list, name='blog_list'),
     path('blog/<slug:slug>/', views.blog_detail, name='blog_detail'),
