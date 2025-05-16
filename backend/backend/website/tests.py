308b7eb13c1005ec1768a9bbc0d2325c106843ca
ricardocanedo <ricardocanedo@live.com>
2025-05-16 08:29:19 -0300

    fix: home test

--- backend/backend/website/tests.py
+++ backend/backend/website/tests.py
@@ -7,7 +7,7 @@ class WebsiteViewTest(TestCase):
     def test_home_view(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Defesa médica de alta complexidade')
+        self.assertContains(response, 'Atuação especializada em defesa médica complexa')
 
     def test_sobre_view(self):
         response = self.client.get(reverse('sobre'))
