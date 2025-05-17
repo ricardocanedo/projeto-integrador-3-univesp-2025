ba51da94e21f99c291ba00d015598a8560b4c44d
ricardocanedo <ricardocanedo@live.com>
2025-05-17 14:17:45 -0300

    router basename

--- cms/src/App.tsx
+++ cms/src/App.tsx
@@ -12,7 +12,7 @@ import './App.css'
 
 function App() {
     return (
-      <Router>
+      <Router basename="/cms">
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
