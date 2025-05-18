8ca14494a31d61dcfc12f325ae572a8f0599bb59
ricardocanedo <ricardocanedo@live.com>
2025-05-18 10:08:48 -0300

    fix path for collectstatic

--- backend/backend/core/settings.py
+++ backend/backend/core/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'backend' / 'website' / 'static',  # ← ajuste aqui se necessário
+    BASE_DIR / 'website' / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'backend' / 'static'
 
