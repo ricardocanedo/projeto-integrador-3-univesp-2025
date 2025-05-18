13ca0672e34d9a8d498fdf2d0c62a0333fbbeee4
ricardocanedo <ricardocanedo@live.com>
2025-05-18 09:53:52 -0300

    change statics

--- backend/backend/core/settings.py
+++ backend/backend/core/settings.py
@@ -129,13 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    BASE_DIR / 'website' / 'static',
+    BASE_DIR / 'backend' / 'website' / 'static',  # ← ajuste aqui se necessário
 ]
+STATIC_ROOT = BASE_DIR / 'backend' / 'static'
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'backend' / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
